b83ad67695450cd4e104b620323aa42d0872d7aa
Catherine Bronzo <cbronzo@mac.com>
2018-06-20 00:02:44 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,11 @@
 def prime?(num)
  return false if n <= 1
- if
- 
-  elsif
-    (2..(num - 1)).each do |i|
+ (2..(num - 1)).each do |i|
     num % i == 0
       if num % i == 0
-+       return false
+       return false
       end
-    end
+  end
   else
     return true
   end
